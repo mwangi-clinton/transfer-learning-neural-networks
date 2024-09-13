fbafef3e8299f738c3cc17c6a4b55ce33f87ddd2
mwangi-clinton <clintonmwangi17@gmail.com>
2024-09-13 12:46:20 +0000

    helper functions

--- utils/helper_functions.py
+++ utils/helper_functions.py
@@ -5,6 +5,36 @@ from torchvision import transforms
 from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader, random_split
 from torchvision import transforms
+import os
+import shutil
+
+def  build_dataset(source_dir,dest_dir):
+  #create the validation and traning folders in the destination folder
+    os.makedirs(os.path.join(train_dir, dest_dir), exist_ok=True)
+    os.makedirs(os.path.join(val_dir, dest_dir), exist_ok=True)
+    for class_folder in os.listdir(source_dir):
+        full_class_path = os.path.join(source_dir, class_folder)
+    
+    # Get all images in the current class folder
+        image_files = [f for f in os.listdir(full_class_path) if os.path.isfile(os.path.join(full_class_path, f))]
+    
+    # Split the images into 80% train and 20% validation
+        train_files, val_files = train_test_split(image_files, test_size=0.2, random_state=42)
+    
+    # Create directories for this class in train and val directories
+        os.makedirs(os.path.join(train_dir, class_folder), exist_ok=True)
+        os.makedirs(os.path.join(val_dir, class_folder), exist_ok=True)
+    
+    # Move train files to train directory
+        for file in train_files:
+            shutil.copy(os.path.join(full_class_path, file), os.path.join(train_dir, class_folder, file))
+    
+    # Move validation files to validation directory
+        for file in val_files:
+            shutil.copy(os.path.join(full_class_path, file), os.path.join(val_dir, class_folder, file))
+
+
+
 
 def create_dataloaders(data_folder):
     transform = transforms.Compose([
