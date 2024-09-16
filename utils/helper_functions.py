13e7661236db0d5b5372492dff5fda0cbc14581e
mwangi-clinton <clintonmwangi17@gmail.com>
2024-09-16 07:34:15 +0000

    helper changes

--- utils/helper_functions.py
+++ utils/helper_functions.py
@@ -33,17 +33,24 @@ def  build_dataset(source_dir,dest_dir):
         for file in val_files:
             shutil.copy(os.path.join(full_class_path, file), os.path.join(val_dir, class_folder, file))
 
-    transform = transforms.Compose(
+    trainTransform = transforms.Compose(
       [
-    transforms.Resize((224, 224)),  # Resize images to 224x224 (or any size required by your model)
-    transforms.ToTensor(),          # Convert image to tensor
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # Normalization values for pre-trained models
+        	transforms.RandomHorizontalFlip(),
+	        transforms.RandomRotation(90),
+          transforms.Resize((224, 224)),  # Resize images to 224x224 (or any size required by your model)
+          transforms.ToTensor(),          # Convert image to tensor
+          transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # Normalization values for pre-trained models
 ]
     )
+    valTransform=([
+      transforms.Resize((224,224)),
+      transforms.ToTensor(),
+      transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
 
     # Load train and validation datasets
-    train_dataset = ImageFolder(root=train_dir, transform=transform)
-    val_dataset = ImageFolder(root=val_dir, transform=transform)
+    train_dataset = ImageFolder(root=train_dir, transform=trainTransform)
+    val_dataset = ImageFolder(root=val_dir, transform=valTransform)
 
     # Data loaders for train and validation datasets
     train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
